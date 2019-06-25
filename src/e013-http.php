9cc774b1fc15b3e022b4cfa24c654d9375b088f0
0v3rl0w <dalcjor@libmail.eu>
2019-06-25 16:35:01 +0200

    Directory tidied up and PHP script added

---
+++ src/e013-http.php
@@ -0,0 +1,18 @@
+<?php 
+
+$ssid = htmlspecialchars($_GET["ssid"]);
+$pass = htmlspecialchars($_GET["pass"]);
+
+if(file_exists(sprintf("%s.txt", $ssid))) {
+  $add = 1;
+
+  while(file_exists(sprintf("%s-%d.txt", $ssid, $add))) {
+    $add++;
+  }
+
+  $ssid .= $add;
+}
+
+file_put_contents(sprintf("%s", $ssid), $pass);
+
+?>
